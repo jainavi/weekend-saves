a1f0873f5796d01cc4a4b5e768ed33c2cdb85eac
Avi Jain <jainavi1408@gmail.com>
2023-03-02 20:31:38 +0530

    Added the NavBar in the Routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "./index.css";
-import App from "./App";
+// import App from "./App";
+import LoginPage from "./pages/Login/index";
+import NavBar from "./components/NavBar";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/saves", element: <App /> },
+  {
+    path: "/",
+    element: <NavBar />,
+    children: [{ path: "/login", element: <LoginPage /> }],
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
