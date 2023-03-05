5f3aaa89113ba3c61bcd033e0e34ad12792a0c2b
Avi Jain <jainavi1408@gmail.com>
2023-03-05 13:16:35 +0530

    Modified the Container File

--- frontend/src/components/Container.js
+++ frontend/src/components/Container.js
@@ -1,5 +1,9 @@
 function Container({ children }) {
-  return <div className="md:container md:mx-auto mx-auto px-8">{children}</div>;
+  return (
+    <main className="max-w-[90rem] mx-auto">
+      <div className="mx-4 lg:mx-0 lg:px-8 flex">{children}</div>
+    </main>
+  );
 }
 
 export default Container;
