0c88d86ac7561db410d0202e680529e87b3e0c4e
Avi Jain <jainavi1408@gmail.com>
2023-03-06 20:47:43 +0530

    Added Functionality to Switch to Register Form

--- frontend/src/pages/Login/LoginForm.js
+++ frontend/src/pages/Login/LoginForm.js
@@ -1,8 +1,14 @@
+import { useDispatch } from "react-redux";
+
+import { showRegisterForm } from "../../slices/authSlice";
+
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import Label from "../../components/Label";
 
 function LoginForm() {
+  const dispatch = useDispatch();
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
@@ -12,19 +18,26 @@ function LoginForm() {
       <h1 className="text-4xl font-extrabold z-10 mx-auto">Login</h1>
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col items-center gap-5"
+        className="flex-1 flex flex-col items-center gap-5 justify-center"
       >
         <Label htmlFor="username">Username:</Label>
-        <Input name="Username" />
+        <Input name="username" placeholder="Username" />
         <Label htmlFor="password">Password:</Label>
-        <Input name="Password" />
-        <a className="self-end font-form text-gray hover:underline hover:text-grayL">
+        <Input name="password" placeholder="Password" />
+        <a className="self-end font-form font-light hover:font-normal text-gray hover:underline hover:text-grayL">
           Forgot Password?
         </a>
         <Button type="submit" color="btn-accent" size="btn-block">
           Login
         </Button>
-        <a className="font-form text-accent hover:underline">Create Account</a>
+        <p
+          className="font-form text-accent hover:underline font-extralight hover:font-light hover:cursor-pointer"
+          onClick={() => {
+            dispatch(showRegisterForm());
+          }}
+        >
+          Create Account
+        </p>
       </form>
     </>
   );
