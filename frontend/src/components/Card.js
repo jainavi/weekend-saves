b95e13dceb6300804ffc00660e10036ee2f4133f
Avi Jain <jainavi1408@gmail.com>
2023-03-06 16:49:34 +0530

    Changed the Padding -changed the y padding to 6 -changed the x padding to 8

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,7 +1,7 @@
 function Card({ color, size, position, extra, children }) {
   return (
     <div
-      className={`p-6 rounded-md flex ${color} ${size} ${position} ${extra}`}
+      className={`py-6 px-8 rounded-md flex ${color} ${size} ${position} ${extra}`}
     >
       {children}
     </div>
