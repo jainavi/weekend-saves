28cca8ed8eb7475f956aba9b934caf6010a8f67f
Avi Jain <jainavi1408@gmail.com>
2023-03-06 20:47:58 +0530

    Added a Register Form

---
+++ frontend/src/pages/Login/RegisterForm.js
@@ -0,0 +1,31 @@
+import Label from "../../components/Label";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+function RegisterForm() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <h1 className="text-4xl font-extrabold z-10 mx-auto">Register</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="flex-1 flex flex-col items-center gap-5 justify-center"
+      >
+        <Label htmlFor="email">Email:</Label>
+        <Input name="email" placeholder="Email" />
+        <Label htmlFor="password">Password:</Label>
+        <Input name="password" placeholder="Password" />
+        <Label htmlFor="phone number">Phone Number:</Label>
+        <Input name="phoneNumber" placeholder="Phone Number" />
+        <Button type="submit" color="btn-accent" size="btn-block" extra="mt-3">
+          Register
+        </Button>
+      </form>
+    </>
+  );
+}
+
+export default RegisterForm;
