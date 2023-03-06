95f69affda09b4d024638a8024ee7e82ab2921ba
Avi Jain <jainavi1408@gmail.com>
2023-03-06 20:46:46 +0530

    Added Placeholder Prop to Input Component

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,10 +1,10 @@
-function InputFeild({ name }) {
+function InputFeild({ name, placeholder }) {
   return (
     <input
       type="text"
       id={name}
       name={name}
-      placeholder={name}
+      placeholder={placeholder}
       className="transition-all font-form text-base tracking-wide w-full pb-2 placeholder:text-gray hover:placeholder:text-grayL focus:placeholder:text-grayL bg-secondary border-solid border-b-2 border-b-gray focus:border-none focus:outline focus:outline-offset-8 focus:outline-grayL focus:outline-2 focus:rounded-sm focus:pb-0"
     ></input>
   );
