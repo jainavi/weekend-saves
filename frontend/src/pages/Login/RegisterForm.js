3ae72cd44c3aaffb337847429cec0ded4236f44e
Avi Jain <jainavi1408@gmail.com>
2023-03-08 18:57:22 +0530

    Changed Register Form Layout and Added Last Name Feild

--- frontend/src/pages/Login/RegisterForm.js
+++ frontend/src/pages/Login/RegisterForm.js
@@ -12,19 +12,34 @@ function RegisterForm() {
       <h1 className="text-4xl font-extrabold z-10 mx-auto">Register</h1>
       <form
         onSubmit={handleSubmit}
-        className="mt-8 flex-1 flex flex-col items-center gap-5 justify-center"
+        className="mt-8 flex-1 grid grid-cols-2 gap-5"
       >
         <Label htmlFor="email">Email:</Label>
-        <Input name="email" placeholder="Email" />
+        <Input name="email" placeholder="Email" extra="col-span-2" />
         <Label htmlFor="password">Password:</Label>
-        <Input name="password" placeholder="Password" />
+        <Input name="password" placeholder="Password" extra="col-span-2" />
         <Label htmlFor="confirmPassword">Confirm Password:</Label>
-        <Input name="confirmPassword" placeholder="Confirm Password" />
+        <Input
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          extra="col-span-2"
+        />
         <Label htmlFor="name">Name:</Label>
-        <Input name="phoneNumber" placeholder="Phone Number" />
-        <Label htmlFor="name">Name:</Label>
-        <Input name="name" placeholder="Name" />
-        <Button type="submit" color="btn-accent" size="btn-block">
+        <Input
+          name="phoneNumber"
+          placeholder="Phone Number"
+          extra="col-span-2"
+        />
+        <Label htmlFor="firstName">First Name:</Label>
+        <Input name="firstName" placeholder="First Name" />
+        <Label htmlFor="firstName">Last Name:</Label>
+        <Input name="lastName" placeholder="Last Name" />
+        <Button
+          type="submit"
+          color="btn-accent"
+          size="btn-block"
+          extra="col-span-2"
+        >
           Register
         </Button>
       </form>
