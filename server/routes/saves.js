7eae286a46c77b82a94801fdf873d2126bdbceed
Avi Jain <jainavi1408@gmail.com>
2023-03-08 23:45:01 +0530

    Added Route Protection

--- server/routes/saves.js
+++ server/routes/saves.js
@@ -1,22 +1,23 @@
 const express = require("express");
 
+const isAuth = require("../middleware/isAuth");
 const { getAllPosts, deleteSave, postSave } = require("../controllers/saves");
 
 const router = express.Router();
 
 // GET /saves/
-router.get("/", getAllPosts);
+router.get("/", isAuth, getAllPosts);
 
 // GET /saves/:saveId
-router.get("/:saveId");
+router.get("/:saveId", isAuth);
 
 // POST /saves/post
-router.post("/post", postSave);
+router.post("/post", isAuth, postSave);
 
 // PUT /saves/:saveId
-router.put("/:saveId");
+router.put("/:saveId", isAuth);
 
 // DELETE /save/:saveId
-router.delete("/:saveId", deleteSave);
+router.delete("/:saveId", isAuth, deleteSave);
 
 module.exports = router;
