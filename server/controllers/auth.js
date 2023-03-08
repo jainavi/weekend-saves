25fbd925ce4fccd3e6db0afd940aa38e47fb1419
Avi Jain <jainavi1408@gmail.com>
2023-03-08 21:51:03 +0530

    Added the SignUp Controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,37 @@
-exports.signUp = (req, res) => {
-  console.log("You have reached signUp route");
+const { validationResult } = require("express-validator/check");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user");
+
+exports.signUp = (req, res, next) => {
+  const validationError = validationResult(req);
+  if (!validationError.isEmpty()) {
+    const error = new Error("Validation Failed");
+    error.statusCode = 422;
+    error.data = validationError.array();
+    return next(error);
+  }
+  const { email, password, phoneNumber, firstName, lastName } = req.body;
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPw) => {
+      const user = new User({
+        email,
+        password: hashedPw,
+        phoneNumber,
+        firstName,
+        lastName,
+        saves: [],
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({ message: "User created" });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      return next(err);
+    });
 };
