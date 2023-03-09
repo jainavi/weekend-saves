6b0d7b166db4d057f359a6be4df5989941ea72a4
Avi Jain <jainavi1408@gmail.com>
2023-03-09 20:56:09 +0530

    Added Animation Using Auto-Animate Library

--- frontend/src/components/ErrorList.js
+++ frontend/src/components/ErrorList.js
@@ -1,12 +1,17 @@
 import { useSelector } from "react-redux";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 import Error from "./Error";
 
 function ErrorList() {
   const { errorArr } = useSelector((store) => store.ui);
+  const [parent, enableAnimations] = useAutoAnimate();
 
   return (
-    <div className="fixed z-20 top-[80px] right-0 w-44 lg:w-52">
+    <div
+      ref={parent}
+      className="fixed px-4 z-20 top-[80px] right-0 w-56 lg:w-52"
+    >
       {errorArr.map((error, index) => (
         <Error key={error.id} id={error.id}>
           {error.msg}
