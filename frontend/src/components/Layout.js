c927e18408245d8cf2c3dacc1dafcd5bf0acb37d
Avi Jain <jainavi1408@gmail.com>
2023-03-09 18:35:01 +0530

    Added the ErrorList

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "./Navbar";
+import ErrorList from "./ErrorList";
 
 function Layout() {
   return (
     <>
       <Navbar />
+      <ErrorList />
       <main>
         <Outlet />
       </main>
