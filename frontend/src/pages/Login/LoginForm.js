bd01b602c2f7993ff8346dbb055f0aeab757c6a3
Avi Jain <jainavi1408@gmail.com>
2023-03-10 19:55:58 +0530

    Added the Login Logic to the Login Form

--- frontend/src/pages/Login/LoginForm.js
+++ frontend/src/pages/Login/LoginForm.js
@@ -1,12 +1,46 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import Label from "../../components/Label";
+import { loginHandler } from "../../util/api";
+import { pushSuccess, pushError } from "../../slices/uiSlice";
+import { setLogin, setLogout } from "../../slices/authSlice";
 
 function LoginForm() {
-  const handleSubmit = (event) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const initialState = { email: "", password: "" };
+  const [formData, setFormData] = useState(initialState);
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    try {
+      const res = await loginHandler(formData);
+      const remainingMilliseconds = 60 * 60 * 1000;
+      dispatch(setLogin(res.data));
+      setAutoLogout(remainingMilliseconds);
+      dispatch(pushSuccess(res.msg));
+      navigate("/home");
+    } catch (err) {
+      dispatch(pushError(err.message));
+      err.data.forEach((msg) => dispatch(pushError(msg)));
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((state) => ({ ...state, [name]: value }));
+  };
+
+  const setAutoLogout = (remainingMilliseconds) => {
+    setTimeout(() => {
+      dispatch(setLogout());
+      dispatch(pushSuccess("Session time out, Please log in again"));
+      navigate("/login");
+    }, remainingMilliseconds);
   };
 
   return (
@@ -16,10 +50,20 @@ function LoginForm() {
         onSubmit={handleSubmit}
         className="flex flex-col items-center gap-5 justify-center"
       >
-        <Label htmlFor="username">Username:</Label>
-        <Input type="text" name="username" placeholder="Username" />
+        <Label htmlFor="email">Email:</Label>
+        <Input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
         <Label htmlFor="password">Password:</Label>
-        <Input type="password" name="password" placeholder="Password" />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
         <a className="self-end font-form font-light hover:font-normal text-gray hover:underline hover:text-grayL">
           Forgot Password?
         </a>
