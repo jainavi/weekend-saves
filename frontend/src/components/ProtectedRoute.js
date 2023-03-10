68e49048dea18a7549743cf319d4fc8220fbcba0
Avi Jain <jainavi1408@gmail.com>
2023-03-10 21:46:43 +0530

    Added the Logic for Route Protection

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,3 +1,16 @@
-function ProtectedRoute() {}
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function ProtectedRoute({ Page }) {
+  const navigate = useNavigate();
+  const { isAuth } = useSelector((state) => state.auth);
+  useEffect(() => {
+    if (!isAuth) {
+      navigate("/login");
+    }
+  });
+  return <Page />;
+}
 
 export default ProtectedRoute;
