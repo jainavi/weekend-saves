6c3e3e70b36a828420e2f7828da8aa5976de3ac5
Avi Jain <jainavi1408@gmail.com>
2023-03-10 12:39:37 +0530

    Added a Default data Property to the Error Obj. Throwen by the Server to Client

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -2,6 +2,6 @@ module.exports = (error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
-  const data = error.data;
+  const data = error.data || [];
   res.status(status).json({ message, data });
 };
