eaa672911ce5a561fa7b1d213c6234b3bc7e85e7
Avi Jain <jainavi1408@gmail.com>
2023-03-10 19:54:30 +0530

    Added the Logic to Auto Logout or Update Auth State if Token is Present

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,9 +1,40 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import Navbar from "./Navbar";
 import StatusList from "./StatusList";
+import { setLogin, setLogout } from "../slices/authSlice";
+import { pushSuccess } from "../slices/uiSlice";
 
 function Layout() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const expiryDate = localStorage.getItem("expiryDate");
+    if (!token || !expiryDate) {
+      return;
+    }
+    if (new Date(expiryDate) <= new Date()) {
+      dispatch(setLogout());
+      return;
+    }
+    const userId = localStorage.getItem("userId");
+    const remainingMilliseconds =
+      new Date(expiryDate).getTime() - new Date().getTime();
+    dispatch(setLogin({ token, userId }));
+    setAutoLogout(remainingMilliseconds);
+  }, []);
+
+  const setAutoLogout = (remainingMilliseconds) => {
+    setTimeout(() => {
+      dispatch(setLogout());
+      dispatch(pushSuccess("Session time out, Please log in again"));
+      navigate("/login");
+    }, remainingMilliseconds + 1000);
+  };
+
   return (
     <>
       <Navbar />
