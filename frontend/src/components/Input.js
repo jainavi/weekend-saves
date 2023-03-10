8f5f32078a17bc0e8df3855359348f1578a3fd73
Avi Jain <jainavi1408@gmail.com>
2023-03-10 12:42:31 +0530

    Added a Type Prop and Invalid State to the InputFeild Component

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,11 +1,15 @@
-function InputFeild({ name, placeholder, extra }) {
+function InputFeild({ invalid, onChange, type, name, placeholder, extra }) {
+  const styling = invalid
+    ? "placeholder:text-error/50 hover:placeholder:text-error border-b-error focus:outline-error text-error"
+    : "placeholder:text-gray hover:placeholder:text-grayL border-b-gray focus:outline-grayL";
   return (
     <input
-      type="text"
+      type={type}
       id={name}
       name={name}
       placeholder={placeholder}
-      className={`transition-all font-form text-base tracking-wide w-full pb-2 placeholder:text-gray hover:placeholder:text-grayL focus:placeholder:text-grayL bg-secondary border-solid border-b-2 border-b-gray focus:border-none focus:outline focus:outline-offset-8 focus:outline-grayL focus:outline-2 focus:rounded-sm focus:pb-0 ${extra}`}
+      className={`transition-all font-form text-base tracking-wide w-full pb-2 ${styling} bg-secondary border-solid border-b-2 focus:border-none focus:outline focus:outline-offset-8 focus:outline-2 focus:rounded-sm focus:pb-0 ${extra}`}
+      onChange={onChange}
     ></input>
   );
 }
