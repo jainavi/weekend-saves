62d2058dee2d48fa9b75ca02f2df102bf401f411
Avi Jain <jainavi1408@gmail.com>
2023-03-24 19:30:33 +0530

    Updated saves model

--- server/models/save.js
+++ server/models/save.js
@@ -3,19 +3,34 @@ const Schema = mongoose.Schema;
 
 const saveSchema = new Schema(
   {
-    url: String,
-    title: String,
-    content: String,
+    url: { type: String, required: true },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
     modifiedContent: {
       type: String,
       default: null,
     },
-    image: String,
-    source: String,
+    image: { type: String, default: "" },
+    source: { type: String, default: "" },
+    ttr: Number,
     creator: {
       type: Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      // required: true, //Make it required
+    },
+    userOptions: {
+      isArchived: {
+        type: Boolean,
+        default: false,
+      },
+      isFavourite: {
+        type: Boolean,
+        default: false,
+      },
+      tags: {
+        type: [String],
+        default: [],
+      },
     },
   },
   { timestamps: true }
