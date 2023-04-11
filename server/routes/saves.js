c81eaf7a2d2a5b999f37b319b64fe28a38481138
Avi Jain <jainavi1408@gmail.com>
2023-04-11 15:32:57 +0530

    Added data validation to the saves route in the server

--- server/routes/saves.js
+++ server/routes/saves.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const isAuth = require("../middleware/isAuth");
 const {
@@ -17,10 +18,26 @@ router.get("/", isAuth, getAllPosts);
 router.get("/:saveId", isAuth);
 
 // POST /saves/post
-router.post("/post", isAuth, postSave);
+router.post(
+  "/post",
+  isAuth,
+  [
+    check("method").equals("URL").withMessage("Invalid method"),
+    check("url").isURL().withMessage("Invalid URL"),
+  ],
+  postSave
+);
 
 // PUT /saves/changeType/:saveId
-router.put("/change-type", isAuth, changeType);
+router.put(
+  "/change-type",
+  isAuth,
+  [
+    check("saveId").not().isEmpty().withMessage("Invalid request"),
+    check("type").isIn(["0", "1", "2"]).withMessage("Invalid request"),
+  ],
+  changeType
+);
 
 // DELETE /save/:saveId
 router.delete("/:saveId", isAuth, deleteSave);
